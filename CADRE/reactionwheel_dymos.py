6b201778323f82da2036f990bd28da8f8c32327a
mebrandt <meghan.e.brandt@nasa.gov>
2025-05-08 16:10:46 -0400

    Rewriting reactionwheel class with dymos.

---
+++ CADRE/reactionwheel_dymos.py
@@ -0,0 +1,63 @@
+import openmdao.api as om
+import jax.numpy as jnp
+import jax.debug
+
+class ReactionWheel(om.JaxExplicitComponent):
+    """
+    Compute reaction wheel power.
+    """
+    # constants
+    V = 4.0
+    a = 4.9e-4
+    b = 4.5e2
+    I0 = 0.017
+
+    def initialize(self):
+        self.options.declare('num_nodes', types=int)
+
+    def setup(self):
+        n = self.options['num_nodes']
+
+        # Inputs
+        self.add_input('w_B', jnp.zeros((n, 3)), units='1/s',
+                       desc='Angular velocity vector in body-fixed frame over time')
+        
+        self.add_input('w_RW', jnp.zeros((n, 3)), units='1/s',
+                       desc='Angular velocity vector of reaction wheel over time')
+
+        self.add_input('T_RW', jnp.zeros((n, 3)), units='N*m',
+                       desc='Torque vector of reaction wheel over time')
+
+        # Outputs
+        self.add_output('P_RW', jnp.ones((n, 3)), units='W',
+                        desc='Reaction wheel power over time')
+        
+        self.add_output('alpha_RW', jnp.zeros((n, 3)), units='1/s**2',
+                        desc='Angular acceleration vector of reaction wheel over time')
+
+        # unit conversion of some kind
+        self.J_RW = 2.8e-5
+
+    def compute_primal(self, w_B, w_RW, T_RW):
+        """
+        Calculate outputs.
+        """
+        P_RW0 = (self.V * (self.a * w_RW[0, :] +
+                          self.b * T_RW[0, :])**2 +
+                          self.V * self.I0)
+        P_RW1 = (self.V * (self.a * w_RW[1, :] +
+                          self.b * T_RW[1, :])**2 +
+                          self.V * self.I0)
+        P_RW2 = (self.V * (self.a * w_RW[2, :] +
+                          self.b * T_RW[2, :])**2 +
+                          self.V * self.I0)
+
+        P_RW = jnp.vstack((P_RW0, P_RW1, P_RW2))
+
+        a2 = jnp.vstack((-w_B[:,2]*w_RW[:,1] + w_B[:,1]*w_RW[:,2],
+                          w_B[:,2]*w_RW[:,0] - w_B[:,0]*w_RW[:,2],
+                         -w_B[:,1]*w_RW[:,0] + w_B[:,0]*w_RW[:,1]))
+        jax.debug.print('{a2}', a2=a2.shape)
+        alpha_RW = -T_RW / self.J_RW
+
+        return P_RW, alpha_RW
---
+++ CADRE/test/test_reactionwheel_dymos.py
@@ -0,0 +1,18 @@
+import unittest
+import openmdao.api as om
+import dymos as dm
+from CADRE.reactionwheel_dymos import ReactionWheel
+
+class TestReactionWheelDymos(unittest.TestCase):
+    def test_simple_prop(self):
+        p = om.Problem()
+        traj = dm.Trajectory()
+        phase = traj.add_phase('phase', dm.Phase(ode_class=ReactionWheel, 
+                       transcription=dm.PicardShooting(num_segments=1, nodes_per_seg=20)))
+        p.model.add_subsystem('traj', traj)
+
+        phase.set_state_options('w_RW', fix_initial=True, rate_source='alpha_RW')
+        phase.add_control('T_RW', opt=False)
+        p.setup()
+
+        dm.run_problem(p, run_driver=False, make_plots=True)
